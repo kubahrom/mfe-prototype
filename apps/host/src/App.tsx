f4fc3904423f806e54e82919e261d51391b9cceb
Jakub Hromádko <kubahromadko0506@gmail.com>
2024-06-08 15:58:17 +0200

    add shared lib, update workflows, run scripts

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -1,4 +1,6 @@
 import { Suspense, lazy } from 'react';
+import { toUp } from '@libs/shared';
+
 import Layout from './components/Layout';
 import { Route, Router, Switch } from 'react-router-dom';
 import { LoadingScreen } from './components/LoadingScreen';
@@ -10,9 +12,12 @@ const AuthLazy = lazy(() => import('./apps/AuthApp'));
 const history = createBrowserHistory();
 
 export default function App() {
+  const test = toUp('shared lib content');
+
   return (
     <Router history={history}>
       <Layout>
+        {test}
         <Suspense fallback={<LoadingScreen />}>
           <Switch>
             <Route path="/test" exact>
---
+++ libs/shared/src/index.ts
@@ -0,0 +1,3 @@
+export function toUp(text: string): string {
+  return text.toUpperCase();
+}
